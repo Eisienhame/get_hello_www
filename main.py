9b578aa83ea3bd9c210ccd5314ba91116f31bb0e
Eisiehame <knurov24@gmail.com>
2023-05-01 15:02:56 +0200

    last fix

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ text_exp = 'Hello, World wide web!'
 class my_sever(BaseHTTPRequestHandler):
 
     def do_POST(self):
+        # c_len = int(self.headers.get('Content-Length'))
+        # client_data = self.rfile.read(c_len)
         pass
 
     def do_GET(self):
+        print(text_exp)
         self.send_response(200)
+
 if __name__ == "__main__":
     webServer = HTTPServer((hostname, severport), my_sever)
-    print('START')
+    print("Server started http://%s:%s" % (hostname, severport))
 
     try:
         webServer.serve_forever()
